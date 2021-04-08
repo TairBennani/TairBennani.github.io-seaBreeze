21c829622c865f88c8dfd4919bc176321839ffa2
TairBennani <tim-r10@yandex.ru>
2021-04-08 06:32:23 +0300

    burger menu defeated

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,36 @@
 
 // document.write("test");
 $(document).ready(function(){
-    (function () {
-        const burger = document.querySelector('.burger');
-        burger.addEventListener('click',() => {
-            burger.classList.toggle('burger_active');
-        });
-    }());
+    // (function () {
+    //     const burger = document.querySelector('.burger');
+    //     burger.addEventListener('click',() => {
+    //         burger.classList.toggle('burger_active');
+    //     });
+    // }());
+
     // document.write("test");
+    $('.burger').toggle(function(){
+        $(this).addClass('burger_active');
+    },function(){
+        $(this).removeClass('burger_active');
+    });
+
     $("#test").text("I've changed");
 
-    
 
+    // $('.burger').click(function(){
+    //    $('.menu').removeClass('menu');
+    // });
+    //
+    // $('.burger').click(function(){
+    //    $('.menu').addClass('menu');
+    // });
+
+    $('.burger').toggle(function(){
+        $('.forNav').css({'visibility' : 'visible'});
+    },function(){
+        $('.forNav').css({'visibility' : 'hidden'});
+    });
 
 
 
