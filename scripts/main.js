d8fc9276a11002a05ff41e29e62f2eaceb31b5aa
TairBennani <tim-r10@yandex.ru>
2021-04-08 04:29:04 +0300

    hide menu/adaptive

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
-(function () {
-    const burger = document.querySelector('.burger');
-    burger.addEventListener('click',() => {
-        burger.classList.toggle('burger_active');
-    });
-}());
+
+// document.write("test");
+$(document).ready(function(){
+    (function () {
+        const burger = document.querySelector('.burger');
+        burger.addEventListener('click',() => {
+            burger.classList.toggle('burger_active');
+        });
+    }());
+    // document.write("test");
+    $("#test").text("I've changed");
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}); // конец ready
